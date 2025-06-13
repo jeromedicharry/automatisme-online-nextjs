9397b0c03b049853cd4cae8729fb3165196a0d65
Jérôme Dicharry <dicharryjerome@gmail.com>
2025-06-13 09:42:58 +0200

    fix reassurance

--- src/components/sections/blocs/BlocReassurance.tsx
+++ src/components/sections/blocs/BlocReassurance.tsx
@@ -17,6 +17,8 @@ const BlocReassurance = ({
   if (!bloc || !reassuranceItems || reassuranceItems.length === 0) return null;
 
   const items = bloc.isAvis ? reassuranceItems.slice(0, 3) : reassuranceItems;
+
+  // Couleurs de fond du bloc principal
   const mobileBgColor =
     bloc.type === 'Fond bleu clair'
       ? 'max-md:bg-primary-light'
@@ -24,21 +26,29 @@ const BlocReassurance = ({
   const bgColor =
     bloc.type === 'Fond bleu clair' ? 'md:bg-primary-light' : 'md:bg-white';
 
+  // Couleurs de fond des pictos (desktop et mobile)
+  const pictoBgColor =
+    bloc.type === 'Fond bleu clair'
+      ? 'bg-white' // Si fond bleu clair, pictos sur fond blanc
+      : 'bg-primary-light'; // Sinon, pictos sur fond bleu clair
+
   return (
     <Container>
       <section
-        className={`w-full md:rounded-[13px] py-8 md:p-[10px] ${bgColor} ${isFooter ? '' : 'mb-12 md:mb-16'}`}
+        className={`w-full rounded-2xl md:rounded-[13px] py-8 md:p-[10px] ${bgColor} ${mobileBgColor} ${isFooter ? '' : 'mb-12 md:mb-16'}`}
       >
-        <div className="grid grid-cols-2 gap-4 max-md:max-w-md mx-auto md:flex justify-around">
+        <div
+          className={`grid grid-cols-2 gap-2 md:gap-4 max-md:max-w-md mx-auto lg:flex ${isFooter ? 'justify-between' : 'justify-around'}`}
+        >
           {items.map((item, index) => (
             <Link
               href={item?.link || ''}
               key={index}
-              className="flex items-center gap-2 text-primary hover:text-primary-dark duration-300"
+              className={`flex items-center gap-2 text-primary hover:text-primary-dark duration-300 ${isFooter ? '' : 'max-md:px-2'}`}
             >
               {item.picto?.node?.sourceUrl ? (
                 <div
-                  className={`flex rounded-[3px] ${mobileBgColor} px-[6px] py-[5px]`}
+                  className={`flex rounded-[3px] ${pictoBgColor} px-[6px] py-[5px]`}
                 >
                   <Image
                     src={item.picto.node.sourceUrl}
@@ -49,7 +59,7 @@ const BlocReassurance = ({
                   />
                 </div>
               ) : null}
-              <span className="text-[0.6875rem] underline text-balance">
+              <span className="text-[13px] md:text-base leading-general underline text-balance font-bold">
                 {item.label}
               </span>
             </Link>
